47fc5d54f45868e222002e9f0582d77e0f014a39
Draxcodes <joelparkinson@live.co.uk>
2022-09-01 03:39:29 +0100

    Initial Edit of template
    Create Layout,
    Add About page
    Update links
    Change name of website
    Change profile image

--- src/links.js
+++ src/links.js
@@ -1,48 +1,44 @@
 export const mainContent = {
-  imgSrc: `/assets/profile.png`,
-  name: `Little Sticks`,
-  tagline: `Web Development Learning Platform`
+  imgSrc: `/assets/profile.jpg`,
+  name: `Drax Links`,
+  tagline: `Made with Astro`,
 };
 
 export const socialLinks = [
   {
-    title: 'Instagram',
-    icon: 'mdi:instagram',
-    url: 'https://www.instagram.com/jaydanurwin/',
+    title: "Blog",
+    icon: "mdi:book",
+    url: "https://joelparkinson.me",
   },
   {
-    title: 'Twitter',
-    icon: 'mdi:twitter  ',
-    url: 'https://twitter.com/littlesticksdev',
+    title: "Twitter",
+    icon: "mdi:twitter  ",
+    url: "https://twitter.com/draxis_official",
   },
   {
-    title: 'YouTube',
-    icon: 'mdi:youtube',
-    url: 'https://youtube.com/c/LittleSticks',
+    title: "YouTube",
+    icon: "mdi:youtube",
+    url: "https://www.youtube.com/channel/UCUbDgS7ZKUger4MIc28GzFQ",
   },
   {
-    title: 'GitHub',
-    icon: 'mdi:github',
-    url: 'https://github.com/littlesticksdev',
-  }
-]
+    title: "GitHub",
+    icon: "mdi:github",
+    url: "https://github.com/DraxCodes",
+  },
+];
 
 export const links = [
   {
-    text: 'Home',
-    url: 'https://littlesticks.dev',
+    text: "Home",
+    url: "/",
     featured: true,
   },
   {
-    text: 'About',
-    url: '/about',
+    text: "About",
+    url: "/about",
   },
   {
-    text: 'Uses',
-    url: '/uses',
+    text: "See my latest blog post!",
+    url: "https://joelparkinson.me",
   },
-  {
-    text: 'See my latest blog post!',
-    url: '/blog'
-  }
-]
+];
---
+++ src/pages/about.md
@@ -0,0 +1,15 @@
+---
+layout: ../layouts/MainLayout.astro
+title: "About Me"
+author: "Joel Parkinson"
+date: "01/09/2022"
+---
+<img
+      class="profile__img"
+      width="128px"
+      height="128px"
+      src="assets/profile.jpg"
+      alt="Profile Image"
+    />
+# About Me
+This is my personal links webpage. I created this with the aim to learn more about <a href="https://astro.build">Astro</a>.
